3e3bc56cb6dc4cc8595f1ce38428e7da4d7b8cc5
yutao <zhongyut1994@qq.com>
2019-07-15 18:33:40 +0800

    适配主页背景不能完全显示

--- app/src/main/java/com/trigletop/networkroutermanager/Bean/Data.java
+++ app/src/main/java/com/trigletop/networkroutermanager/Bean/Data.java
@@ -4,7 +4,7 @@ public class Data {
 
     /*矽昌sdk管理*/
     //后台 app key
-    public static final String appKey = "d67d8ab4f4c10bf22aa353e27879133c";
+    public static final String appKey = "a74d392ea4262c720e413c55224539f7";
 
     //后台 app secret
     public static final String appSecret = "d67d8ab4f4c10bf22aa353e27879133c";
--- app/src/main/java/com/trigletop/networkroutermanager/view/LoginActivity.java
+++ app/src/main/java/com/trigletop/networkroutermanager/view/LoginActivity.java
@@ -1,17 +1,80 @@
 package com.trigletop.networkroutermanager.view;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.trigletop.networkroutermanager.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import sirouter.sdk.siflower.com.locallibrary.siwifiApi.SFException;
+import sirouter.sdk.siflower.com.remotelibrary.Listener.SFObjectResponseListener;
+import sirouter.sdk.siflower.com.remotelibrary.SFUser;
+
 public class LoginActivity extends Activity {
+
+    @BindView(R.id.tv_login)
+    TextView tvLogin;
+    @BindView(R.id.et_name)
+    EditText etName;
+    @BindView(R.id.et_password)
+    EditText etPassword;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
     }
+
+
+    /**
+     * 通过extra登录
+     * loginExtra为找回帐号的唯一信息(可以是手机号、邮箱等信息）。登录成功后返回SFUser对象
+     *
+     * @param context 上下文
+     * @param extra
+     */
+    private void loginByExtra(Context context, String extra) {
+        SFUser.loginByExtra(context, extra, new SFObjectResponseListener<SFUser>() {
+            @Override
+            public void onSuccess(SFUser sfUser) {
+
+            }
+
+            @Override
+            public void onError(SFException e) {
+
+            }
+        });
+    }
+
+
+    /**
+     * 通过loginKey登录
+     * loginKey为用户生成后用于登录的唯一信息，app端进行保存。
+     *
+     * @param context 上下文
+     * @param sfUser  SFUser实例化对象
+     */
+    private void loginByLoginKey(Context context, SFUser sfUser) {
+        SFUser.loginByKey(context, sfUser.getLoginkey(), new SFObjectResponseListener<SFUser>() {
+            @Override
+            public void onSuccess(SFUser sfUser) {
+
+            }
+
+            @Override
+            public void onError(SFException e) {
+
+            }
+        });
+    }
+
 }
