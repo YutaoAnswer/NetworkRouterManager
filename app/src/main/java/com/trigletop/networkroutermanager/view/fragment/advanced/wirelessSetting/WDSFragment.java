06776cccc6541cd8b008093f52f64c879ce753d4
yutao <zhongyut1994@qq.com>
2019-09-16 19:16:35 +0800

    桥接功能

--- app/src/main/java/com/trigletop/networkroutermanager/view/fragment/advanced/wirelessSetting/WDSFragment.java
+++ app/src/main/java/com/trigletop/networkroutermanager/view/fragment/advanced/wirelessSetting/WDSFragment.java
@@ -9,26 +9,28 @@ import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
 import com.trigletop.networkroutermanager.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import io.reactivex.Single;
 import io.reactivex.SingleObserver;
 import io.reactivex.disposables.Disposable;
 import sirouter.sdk.siflower.com.locallibrary.siwifiApi.LocalApi;
-import sirouter.sdk.siflower.com.locallibrary.siwifiApi.param.GetDeviceParam;
-import sirouter.sdk.siflower.com.locallibrary.siwifiApi.param.GetWDSInfoParam;
 import sirouter.sdk.siflower.com.locallibrary.siwifiApi.param.GetWDSScanParam;
-import sirouter.sdk.siflower.com.locallibrary.siwifiApi.ret.GetDeviceRet;
-import sirouter.sdk.siflower.com.locallibrary.siwifiApi.ret.GetWDSInfoRet;
 import sirouter.sdk.siflower.com.locallibrary.siwifiApi.ret.GetWDSScanInfoRet;
 
 public class WDSFragment extends Fragment {
 
     private static final String TAG = WDSFragment.class.getSimpleName();
+
+    @BindView(R.id.rcyWDS)
+    RecyclerView rcyWDS;
 
     private Unbinder unbinder;
     private static LocalApi mLocalApi;
@@ -63,6 +65,8 @@ public class WDSFragment extends Fragment {
 
     private void initView() {
         initHintDialog();
+
+        rcyWDS.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     private void initData() {
@@ -91,46 +95,8 @@ public class WDSFragment extends Fragment {
                 .setCustomView(R.layout.custom_view_choose_network, getContext());
         Button networkOne = niftyDialogBuilder.findViewById(R.id.btn_network_one);
         Button networkTwo = niftyDialogBuilder.findViewById(R.id.btn_network_two);
-        networkOne.setOnClickListener(v -> {
-            // TODO: 19-9-16 getWdsInfoRet
-            Single<GetWDSInfoRet> getWDSInfoRetSingle = mLocalApi.executeApiWithSingleResponse(new GetWDSInfoParam(LocalApi.DEFAULT_APP_API_VERSION), GetWDSInfoRet.class);
-            getWDSInfoRetSingle.subscribe(new SingleObserver<GetWDSInfoRet>() {
-                @Override
-                public void onSubscribe(Disposable d) {
-
-                }
-
-                @Override
-                public void onSuccess(GetWDSInfoRet getWDSInfoRet) {
-                    Log.d(TAG, "onSuccess: " + getWDSInfoRet);
-                }
-
-                @Override
-                public void onError(Throwable e) {
-
-                }
-            });
-        });
-        networkTwo.setOnClickListener(v -> {
-            // TODO: 19-9-16 getWdsScanInfoRet
-            Single<GetWDSScanInfoRet> getWDSScanInfoRetSingle = mLocalApi.executeApiWithSingleResponse(new GetWDSScanParam(LocalApi.DEFAULT_APP_API_VERSION),GetWDSScanInfoRet.class);
-            getWDSScanInfoRetSingle.subscribe(new SingleObserver<GetWDSScanInfoRet>() {
-                @Override
-                public void onSubscribe(Disposable d) {
-
-                }
-
-                @Override
-                public void onSuccess(GetWDSScanInfoRet getWDSScanInfoRet) {
-                    Log.d(TAG, "onSuccess: " + getWDSScanInfoRet);
-                }
-
-                @Override
-                public void onError(Throwable e) {
-
-                }
-            });
-        });
+        networkOne.setOnClickListener(v -> getWDSScan("2.4G"));
+        networkTwo.setOnClickListener(v -> getWDSScan("5G"));
         niftyDialogBuilder.show();
     }
 
@@ -138,6 +104,37 @@ public class WDSFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         unbinder.unbind();
+    }
+
+    private void getWDSScan(String band) {
+        GetWDSScanParam getWDSScanParam = new GetWDSScanParam(LocalApi.DEFAULT_APP_API_VERSION);
+        getWDSScanParam.setBand(band);
+        Single<GetWDSScanInfoRet> getWDSScanInfoRetSingle = mLocalApi.executeApiWithSingleResponse(getWDSScanParam, GetWDSScanInfoRet.class);
+        getWDSScanInfoRetSingle.subscribe(new SingleObserver<GetWDSScanInfoRet>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onSuccess(GetWDSScanInfoRet getWDSScanInfoRet) {
+                switch (band) {
+                    case "2.4G":
+
+                        break;
+                    case "5G":
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        });
     }
 
 }
